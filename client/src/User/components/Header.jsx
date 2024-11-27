290cbba260fa59edbd8a225c0ee62489775a74a0
adithya vijay <adithyavijay99@gmail.com>
2024-11-27 15:27:05 +0530

    change

--- client/src/User/components/Header.jsx
+++ client/src/User/components/Header.jsx
@@ -67,7 +67,7 @@ const Header = (cookies) => {
           visible ? "" : "transform -translate-y-full "
         } `}
       > 
-      now you are being hosted by adithya
+    jkhjkhfkjhsjkfhjksdhfjksdhjkf
         <div className="navbar-start">
           <div className="dropdown ">
             <div
